457066a92822dcea904033cd93b06232560b0dc0
Yustynn <yustynn@gmail.com>
2016-04-09 08:57:28 -0400

    Fix the world

--- sms/urls.py
+++ sms/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^sms/', include('sms.urls')),
+    url(r'^send/', include('send.urls')),
     url(r'^admin/', admin.site.urls),
 ]
